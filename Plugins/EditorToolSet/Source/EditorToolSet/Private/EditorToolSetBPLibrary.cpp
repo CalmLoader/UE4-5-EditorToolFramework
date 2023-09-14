95c766d8fdb4ba7d41027b0b55d88e264be97a40
Calmer <928521376@qq.com>
2023-09-14 14:56:57 +0800

    Add Method SplitBigFile

--- Plugins/EditorToolSet/Source/EditorToolSet/Private/EditorToolSetBPLibrary.cpp
+++ Plugins/EditorToolSet/Source/EditorToolSet/Private/EditorToolSetBPLibrary.cpp
@@ -6,6 +6,7 @@
 #include "EditorUtilitySubsystem.h"
 #include "EditorUtilityWidgetBlueprint.h"
 #include "Interfaces/IPluginManager.h"
+#include "Misc/FileHelper.h"
 
 void UEditorToolSetBPLibrary::OpenBPUtilityByPath(const FString& Path)
 {
@@ -24,38 +25,94 @@ FString UEditorToolSetBPLibrary::ChooseFolderByExplorer()
 {
 	IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get();
 	FString OutputDirectory;
+	OutputDirectory.Empty();
 	const FString DefaultPath = FPaths::ConvertRelativePathToFull(FPaths::ProjectContentDir());
-	UE_LOG(LogTemp, Log, TEXT("DefaultPath: %s"), *DefaultPath);
-	bool successfullySelected = false;
-
 	if (DesktopPlatform)
 	{
 		const void* ParentWindowWindowHandle = FSlateApplication::Get().FindBestParentWindowHandleForDialogs(nullptr);
-
-		successfullySelected = DesktopPlatform->OpenDirectoryDialog(
-			ParentWindowWindowHandle,
-			FString(TEXT("选择目录")),
-			DefaultPath,
-			OutputDirectory
-		);
+		if(DesktopPlatform->OpenDirectoryDialog(ParentWindowWindowHandle, FString(TEXT("选择目录")), DefaultPath, OutputDirectory))
+		{
+		}
 	}
 	else
 	{
 		UE_LOG(LogTemp, Error, TEXT("Cannot find the DesktopPlatform!"));
 	}
-
-	if (!successfullySelected)
-	{
-		return FString(TEXT(""));
-	}
-
-	FPaths::MakePathRelativeTo(OutputDirectory, *DefaultPath);
-	OutputDirectory = TEXT("/Game/") + OutputDirectory;
-
 	return OutputDirectory;
+}
+
+FString UEditorToolSetBPLibrary::ChooseFileByExplorer()
+{
+	IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get();
+	const FString DefaultPath = FPaths::ConvertRelativePathToFull(FPaths::ProjectContentDir());
+	FString OutFilePath;
+	OutFilePath.Empty();
+	if (DesktopPlatform)
+	{
+		const void* ParentWindowWindowHandle = FSlateApplication::Get().FindBestParentWindowHandleForDialogs(nullptr);
+		TArray<FString> OutFiles;
+		if (DesktopPlatform->OpenFileDialog(ParentWindowWindowHandle, TEXT("选择文件"), DefaultPath,TEXT(""), TEXT("*"),
+		                                    EFileDialogFlags::None, OutFiles))
+		{
+			if (OutFiles.Num() == 1)
+			{
+				OutFilePath = FPaths::ConvertRelativePathToFull(OutFiles[0]);
+			}
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot find the DesktopPlatform!"));
+	}
+	return OutFilePath;
 }
 
 FString UEditorToolSetBPLibrary::GetToolSetPluginBasePath()
 {
 	return IPluginManager::Get().FindPlugin(TEXT("EditorToolSet"))->GetBaseDir();
+}
+
+void UEditorToolSetBPLibrary::SplitBigFile(const FString& FilePath, const int64 SingleFileSize)
+{
+	if (!IFileManager::Get().FileExists(*FilePath))
+	{
+		UE_LOG(LogTemp, Error, TEXT("FilePath:%s is not exist or is a directory"), *FilePath);
+		return;
+	}
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	const int64 FileSize = PlatformFile.FileSize(*FilePath);
+	const FString FullPath = FPaths::ConvertRelativePathToFull(FilePath);
+	const FString BaseFilename = FPaths::GetBaseFilename(FullPath);
+	const FString BasePath = FPaths::Combine(FPaths::GetPath(FullPath));
+	const FString Extension = FPaths::GetExtension(FullPath, true);
+	IFileHandle* FileHandle = PlatformFile.OpenRead(*FilePath);
+
+	int32 FileIndex = 1;
+	int64 Offset = 0;
+	while (true)
+	{
+		FString CurrentFilename = BaseFilename + FString::FromInt(FileIndex);
+		FString SplitFilePath = BasePath / CurrentFilename + Extension;
+		IFileHandle* SingleFileHandle = PlatformFile.OpenWrite(*SplitFilePath);
+		int64 Size = SingleFileSize;
+		if (Offset + SingleFileSize >= FileSize)
+		{
+			Size = FileSize - Offset;
+		}
+		TArray<uint8> OutData;
+		OutData.AddUninitialized(Size);
+		bool bRead = FileHandle->Read(OutData.GetData(), Size);
+		bool bWrite = SingleFileHandle->Write(OutData.GetData(), Size);
+		SingleFileHandle->Flush(true);
+		delete SingleFileHandle;
+		if (Size != SingleFileSize)
+		{
+			break;
+		}
+		Offset += SingleFileSize;
+		FileIndex++;
+	}
+
+	FileHandle->Flush();
+	delete FileHandle;
 }
--- Plugins/EditorToolSet/Source/EditorToolSet/Public/EditorToolSetBPLibrary.h
+++ Plugins/EditorToolSet/Source/EditorToolSet/Public/EditorToolSetBPLibrary.h
@@ -13,8 +13,8 @@ UCLASS()
 class EDITORTOOLSET_API UEditorToolSetBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-public:
 
+public:
 	UFUNCTION(BlueprintCallable, Category="EditorToolSet")
 	static void OpenBPUtilityByPath(const FString& Path);
 
@@ -22,5 +22,12 @@ public:
 	static FString ChooseFolderByExplorer();
 
 	UFUNCTION(BlueprintCallable, Category="EditorToolSet")
+	static FString ChooseFileByExplorer();
+
+	UFUNCTION(BlueprintCallable, Category="EditorToolSet")
 	static FString GetToolSetPluginBasePath();
+
+	//切分大文件，例如日志文件
+	UFUNCTION(BlueprintCallable, Category="EditorToolSet")
+	static void SplitBigFile(const FString& FilePath, const int64 SingleFileSize = 1024);
 };
